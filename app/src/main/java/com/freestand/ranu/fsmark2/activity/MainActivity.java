33e5fb90747461629b6f06ae380b0db98434956f
rishabh bhutani <bhutani.rishabh96@gmail.com>
2018-01-07 19:57:13 +0530

    no change...

--- app/src/main/java/com/freestand/ranu/fsmark2/activity/MainActivity.java
+++ app/src/main/java/com/freestand/ranu/fsmark2/activity/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setLoginCallback();
         setProfileTracker();
+
         setAccessTokenTracker();
     }
 
